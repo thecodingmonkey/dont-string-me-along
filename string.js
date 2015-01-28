1a42e0f4207dade09eebff2b0b98858610a22e66
Mika <mikadewitz@gmail.com>
2015-01-27 19:47:51 -1000

    changes

---
+++ string.js
@@ -0,0 +1,13 @@
+function missingNum(){
+ var search = /i/;
+ var string = "9899100101013104015";
+ for (var i = 0; i<string.length; i++){
+  if (search.test(string))
+  }
+  return
+ }
+};
+function swap(){
+  var answer = [];
+  answer.push.reverse(string.split(" "));
+}
